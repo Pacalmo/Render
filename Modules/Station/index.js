a12d2944fed548d53abcea9a15240f5ae898a218
B <B@%20>
2020-11-01 21:08:51 -0800

    update

---
+++ Modules/Station/index.js
@@ -0,0 +1,18 @@
+
+
+
+var Station = require ("http").createServer ((req, res) => {
+	const { url, method } = req;
+	if (url === "/") {
+
+
+	}
+});
+
+Station.listen (() => {
+	const { port, address, family } = Station.address();
+	console.log (`Station ready at ${ address }:${ port }`);
+
+
+	Resolve ();
+});
